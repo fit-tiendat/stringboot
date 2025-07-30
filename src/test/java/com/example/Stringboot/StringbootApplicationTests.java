96b9888e6515e4b4adb31fdca276f6235d3080a6
Tien Dat <22704321.dat@student.iuh.edu.vn>
2025-07-30 10:07:39 +0700

    Initial commit

---
+++ src/test/java/com/example/Stringboot/StringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Stringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
